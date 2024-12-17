7abd8dd59d9875647e034f1fa67546d9818b1b26
Anil Pal <anilcodes01@gmail.com>
2024-12-17 21:34:24 +0530

    fixing errors

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,15 +4,18 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://wanderwise-yvxl.vercel.app',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
       }
     }
+  },
+  server: {
+    port: 5173
   }
 })
