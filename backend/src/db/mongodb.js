9ab79a2ea25af491a9db410f274fb530a012c505
Anil Pal <anilcodes01@gmail.com>
2024-12-17 18:42:17 +0530

    fixing errors

--- backend/src/db/mongodb.js
+++ backend/src/db/mongodb.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectToDB = async () => {
   try {
-    await mongoose.connect(process.env.DATABASE_URL);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("MongoDB Connected");
   } catch (err) {
     console.error("Mongo Error:", err.message);
