a411bea2ae5c0d35d2c1d42a20f35776f08c8c38
Anil Pal <anilcodes01@gmail.com>
2024-12-17 22:19:42 +0530

    fixing cors error

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,7 +11,7 @@ connectToDB()
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5173', "https://wanderwise-t34b.vercel.app"],
+  origin: ['http://localhost:5173', "https://wanderwise-tl1p.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
